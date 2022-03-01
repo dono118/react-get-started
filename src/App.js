5520ea7fd5cdb97ea671505b680059922e2b455e
dono118 <dono118@126.com>
2022-03-01 20:22:13 +0800

    comment组件

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import "./App.css";
-import Greeting from "./greeting";
+import './App.css'
+import Comment from './comment'
 
 function App() {
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64'
+    }
+  }
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <Greeting />
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
