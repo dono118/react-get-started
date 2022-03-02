5ae7296070f9103d7e71939d480355cc66d59db3
dono118 <dono118@126.com>
2022-03-02 21:04:16 +0800

    clock组件

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css'
-import Comment from './comment'
+import Clock from './clock'
 
 function App() {
-  const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-      name: 'Hello Kitty',
-      avatarUrl: 'https://placekitten.com/g/64/64'
-    }
-  }
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <Comment
-        date={comment.date}
-        text={comment.text}
-        author={comment.author}
-      />
+      <Clock />
     </div>
   )
 }
