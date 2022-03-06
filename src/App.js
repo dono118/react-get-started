2cbbc7e8dd79bd834eed97bb82078e32dc70e2a4
dono118 <dono118@126.com>
2022-03-06 22:49:42 +0800

    列表&key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css'
-import Warning from '@/views/warning'
+import Blog from '@/views/blog'
 
 function App() {
+  const posts = [
+    { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+    { id: 2, title: 'Installation', content: 'You can install React from npm.' }
+  ]
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <Warning />
+      <Blog posts={posts} />
     </div>
   )
 }
