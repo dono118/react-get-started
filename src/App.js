075706713bea6373bec6bb97aadf701067c568be
dono118 <dono118@126.com>
2022-03-08 22:01:32 +0800

    表单-textarea 标签

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css'
-import NameForm from '@/views/forms/NameForm'
+import EssayForm from '@/views/forms/EssayForm'
 
 function App() {
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <NameForm />
+      <EssayForm />
     </div>
   )
 }
