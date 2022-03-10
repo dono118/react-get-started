21c4af9173f88a6e6d05a2e45804bbdcfc9d3019
dono118 <dono118@126.com>
2022-03-10 22:25:15 +0800

    表单-处理多个输入

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css'
-import FlavorForm from '@/views/forms/FlavorForm'
+import Reservation from '@/views/forms/Reservation'
 
 function App() {
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <FlavorForm />
+      <Reservation />
     </div>
   )
 }
