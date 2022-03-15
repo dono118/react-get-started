905db65aa3cd9c65a9881a18f089082d472ceef2
dono118 <dono118@126.com>
2022-03-15 21:39:58 +0800

    可搜索的产品数据表格

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css'
-import SignUpDialog from '@/views/signUpDialog'
+import FilterableProductTable from '@/views/filterableProductTable'
 
 function App() {
+  const PRODUCTS = [
+    {
+      category: 'Sporting Goods',
+      price: '$49.99',
+      stocked: true,
+      name: 'Football'
+    },
+    {
+      category: 'Sporting Goods',
+      price: '$9.99',
+      stocked: true,
+      name: 'Baseball'
+    },
+    {
+      category: 'Sporting Goods',
+      price: '$29.99',
+      stocked: false,
+      name: 'Basketball'
+    },
+    {
+      category: 'Electronics',
+      price: '$99.99',
+      stocked: true,
+      name: 'iPod Touch'
+    },
+    {
+      category: 'Electronics',
+      price: '$399.99',
+      stocked: false,
+      name: 'iPhone 5'
+    },
+    {
+      category: 'Electronics',
+      price: '$199.99',
+      stocked: true,
+      name: 'Nexus 7'
+    }
+  ]
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <SignUpDialog />
+      <FilterableProductTable products={PRODUCTS} />
     </div>
   )
 }
