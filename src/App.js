ff123b7c261df632074cefe0d2baffd9942b29e0
dono118 <dono118@126.com>
2022-03-18 21:06:09 +0800

    高阶组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css'
-import CustomTextInput from '@/views/customTextInput'
+import HocDemo from '@/views/hocDemo'
 
 function App() {
   return (
     <div className="App">
       <h1>Learn React</h1>
-      <CustomTextInput />
+      <HocDemo />
     </div>
   )
 }
