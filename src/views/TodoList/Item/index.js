a1cb06851e81f16aa989025c6cb5014d5d8e76d1
dono118 <dono118@126.com>
2022-03-24 20:20:51 +0800

    TodoList-鼠标移入移出效果

--- src/views/TodoList/Item/index.js
+++ src/views/TodoList/Item/index.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react'
 import './style.less'
 
 export default class index extends Component {
+  state = {
+    mouse: false
+  }
+  // 处理鼠标的移入移出事件
+  handleMouse = flag => {
+    return () => {
+      this.setState({
+        mouse: flag
+      })
+    }
+  }
+
   render() {
     const { name, done } = this.props
+    const { mouse } = this.state
     return (
-      <li>
+      <li
+        onMouseEnter={this.handleMouse(true)}
+        onMouseLeave={this.handleMouse(false)}
+        style={{ backgroundColor: mouse ? '#ddd' : '#fff' }}
+      >
         <label>
           <input type="checkbox" defaultChecked={done} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: 'none' }}>
+        <button
+          className="btn btn-danger"
+          style={{ display: mouse ? 'block' : 'none' }}
+        >
           删除
         </button>
       </li>
