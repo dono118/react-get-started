194f0add69ec6e95e10a8fe0623a91dfcee20ab8
dono118 <dono118@126.com>
2022-03-28 21:31:23 +0800

    对props进行限制

--- src/views/TodoList/Header/index.js
+++ src/views/TodoList/Header/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { nanoid } from 'nanoid'
 import './style.less'
 
 export default class index extends Component {
+  // 对接受的props进行类型、必要性的限制
+  static propTypes = {
+    addTodo: PropTypes.func.isRequired
+  }
+
   // 处理input enter事件
   handleKeyUp = event => {
     // 解构赋值获取keyCode, target
--- src/views/TodoList/Item/index.js
+++ src/views/TodoList/Item/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './style.less'
 
 export default class index extends Component {
+  // 对接受的props进行类型、必要性的限制
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    done: PropTypes.bool.isRequired,
+    updateTodo: PropTypes.func.isRequired
+  }
+
   state = {
     mouse: false
   }
