720b26a05625c12bec0ad530684f2ef72eb8e6c4
dono118 <dono118@126.com>
2022-04-01 21:52:39 +0800

    github搜索案例-axios发送请求和解决跨域问题

--- src/views/searchDemo/Search/index.js
+++ src/views/searchDemo/Search/index.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Search extends Component {
+  search = () => {
+    // 获取用户输入
+    const {
+      keywordElement: { value: keyword }
+    } = this
+    console.log(keyword)
+    // 发送网络请求
+    axios.get(`http://localhost:3000/api/search/users2?q=${keyword}`).then(
+      res => {
+        // 请求成功
+        console.log('success==>', res.data)
+      },
+      err => {
+        // 请求失败
+        console.log('error==>', err)
+      }
+    )
+  }
+
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />
-          &nbsp;<button>Search</button>
+          <input
+            ref={c => (this.keywordElement = c)}
+            type="text"
+            placeholder="enter the name you search"
+          />
+          &nbsp;
+          <button onClick={this.search}>Search</button>
         </div>
       </section>
     )
