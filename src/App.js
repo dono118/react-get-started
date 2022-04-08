835749d1d26e25c4017e8ff7e8239e0cba758ac1
dono118 <dono118@126.com>
2022-04-08 21:29:28 +0800

    路由的基本使用

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import './App.css'
-import SearchDemoPubSub from '@/views/searchDemoPubSub'
+import { Link, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learn React</h1>
-      <SearchDemoPubSub />
+    <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-xs-8">
+          <div className="page-header">
+            <h2>React Router Demo</h2>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            {/* 导航区-a标签改为Link标签 */}
+            <Link className="list-group-item" to="/home">
+              Home
+            </Link>
+            <Link className="list-group-item" to="/about">
+              About
+            </Link>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body">
+              {/* 展示区-Route标签进行路径的匹配 */}
+              <Route path="/home" component={Home} />
+              <Route path="/about" component={About} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
