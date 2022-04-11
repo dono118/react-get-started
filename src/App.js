774d553f0d0681625b509f826e15dbee65a2ab64
dono118 <dono118@126.com>
2022-04-11 20:07:52 +0800

    封装NavLink组件

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { NavLink, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Header from './components/Header' // Header是一般组件
+import ENavLink from './components/ENavLink'
 import Home from './views/Home' // Home是路由组件
 import About from './views/About' // About是路由组件
 import './App.css'
@@ -16,20 +17,8 @@ function App() {
         <div className="col-xs-2 col-xs-offset-2">
           <div className="list-group">
             {/* 导航区-a标签改为NavLink标签 */}
-            <NavLink
-              activeClassName="nl-active"
-              className="list-group-item"
-              to="/home"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              activeClassName="nl-active"
-              className="list-group-item"
-              to="/about"
-            >
-              About
-            </NavLink>
+            <ENavLink to="/home">Home</ENavLink>
+            <ENavLink to="/about">About</ENavLink>
           </div>
         </div>
         <div className="col-xs-6">
