9c3b2b0243360c126402c3cb7ab9b302461114f7
dono118 <dono118@126.com>
2022-05-06 21:21:28 +0800

    回退和前进

--- src/views/Home/Message/index.js
+++ src/views/Home/Message/index.js
@@ -16,6 +16,21 @@ export default class Message extends Component {
     this.props.history.replace(`/home/message/detail/${id}`)
   }
 
+  // 回退
+  back = () => {
+    this.props.history.goBack()
+  }
+
+  // 前进
+  forward = () => {
+    this.props.history.goForward()
+  }
+
+  // 1 前进一步 -1 后退一步
+  go = () => {
+    this.props.history.go(-1)
+  }
+
   render() {
     const { msgArr } = this.state
     return (
@@ -51,12 +66,13 @@ export default class Message extends Component {
         <hr />
         {/* 声明接收params参数 */}
         <Route path="/home/message/detail/:id" component={Detail} />
-
         {/* search参数无需声明接收 */}
         {/* <Route path="/home/message/detail" component={Detail} /> */}
-
         {/* state参数无需声明接收 */}
         {/* <Route path="/home/message/detail" component={Detail} /> */}
+        <button onClick={this.back}>回退</button>&nbsp;
+        <button onClick={this.forward}>前进</button>&nbsp;
+        <button onClick={this.go}>Go</button>
       </div>
     )
   }
