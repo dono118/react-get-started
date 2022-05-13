6b8faf9164f3b62f669e44fe7e8675fadb441c9a
dono118 <dono118@126.com>
2022-05-13 21:13:42 +0800

    按钮尺寸

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
-import React from 'react'
-import { Button } from 'antd'
+import React, { useState } from 'react'
+import { Button, Radio } from 'antd'
+import { DownloadOutlined } from '@ant-design/icons'
 import 'antd/dist/antd.min.css'
 
 const App = () => {
+  const [size, setSize] = useState('large')
+  const handleSizeChange = e => {
+    setSize(e.target.value)
+  }
+
   return (
     <>
-      <Button type="primary">Primary Button</Button>
-      <Button>Default Button</Button>
-      <Button type="dashed">Dashed Button</Button>
+      <Radio.Group value={size} onChange={handleSizeChange}>
+        <Radio.Button value="large">Large</Radio.Button>
+        <Radio.Button value="default">Default</Radio.Button>
+        <Radio.Button value="small">Small</Radio.Button>
+      </Radio.Group>
       <br />
-      <Button type="text">Text Button</Button>
-      <Button type="link">Link Button</Button>
+      <br />
+      <Button type="primary" size={size}>
+        Primary
+      </Button>
+      <Button size={size}>Default</Button>
+      <Button type="dashed" size={size}>
+        Dashed
+      </Button>
+      <br />
+      <Button type="link" size={size}>
+        Link
+      </Button>
+      <br />
+      <Button type="primary" icon={<DownloadOutlined />} size={size} />
+      <Button
+        type="primary"
+        shape="circle"
+        icon={<DownloadOutlined />}
+        size={size}
+      />
+      <Button
+        type="primary"
+        shape="round"
+        icon={<DownloadOutlined />}
+        size={size}
+      />
+      <Button
+        type="primary"
+        shape="round"
+        icon={<DownloadOutlined />}
+        size={size}
+      >
+        Download
+      </Button>
+      <Button type="primary" icon={<DownloadOutlined />} size={size}>
+        Download
+      </Button>
     </>
   )
 }
-
 export default App
