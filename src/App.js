cb6ab3d82f48a5b843ea1bbd302d5c3bd559b6ec
dono118 <dono118@126.com>
2022-05-28 10:56:13 +0800

    步骤条二

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { Steps } from 'antd'
+import {
+  LoadingOutlined,
+  SmileOutlined,
+  SolutionOutlined,
+  UserOutlined
+} from '@ant-design/icons'
 import 'antd/dist/antd.min.css'
 
 const { Step } = Steps
 
 const App = () => {
   return (
-    <Steps current={1}>
-      <Step title="Finished" description="This is a description." />
-      <Step
-        title="In Progress"
-        subTitle="Left 00:00:08"
-        description="This is a description."
-      />
-      <Step title="Waiting" description="This is a description." />
+    <Steps>
+      <Step status="finish" title="Login" icon={<UserOutlined />} />
+      <Step status="finish" title="Verification" icon={<SolutionOutlined />} />
+      <Step status="process" title="Pay" icon={<LoadingOutlined />} />
+      <Step status="wait" title="Done" icon={<SmileOutlined />} />
     </Steps>
   )
 }
