902bf2e29e4d703261c77613951e3aecf878594c
dono118 <dono118@126.com>
2022-05-29 10:23:23 +0800

    步骤切换

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import { Steps } from 'antd'
-import {
-  LoadingOutlined,
-  SmileOutlined,
-  SolutionOutlined,
-  UserOutlined
-} from '@ant-design/icons'
+import { Button, message, Steps } from 'antd'
+import { useState } from 'react'
 import 'antd/dist/antd.min.css'
 
 const { Step } = Steps
+const steps = [
+  {
+    title: 'First',
+    content: 'First-content'
+  },
+  {
+    title: 'Second',
+    content: 'Second-content'
+  },
+  {
+    title: 'Last',
+    content: 'Last-content'
+  }
+]
 
 const App = () => {
+  const [current, setCurrent] = useState(0)
+
+  const next = () => {
+    setCurrent(current + 1)
+  }
+
+  const prev = () => {
+    setCurrent(current - 1)
+  }
+
   return (
-    <Steps>
-      <Step status="finish" title="Login" icon={<UserOutlined />} />
-      <Step status="finish" title="Verification" icon={<SolutionOutlined />} />
-      <Step status="process" title="Pay" icon={<LoadingOutlined />} />
-      <Step status="wait" title="Done" icon={<SmileOutlined />} />
-    </Steps>
+    <>
+      <Steps current={current}>
+        {steps.map(item => (
+          <Step key={item.title} title={item.title} />
+        ))}
+      </Steps>
+      <div className="steps-content">{steps[current].content}</div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button
+            type="primary"
+            onClick={() => message.success('Processing complete!')}
+          >
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button
+            style={{
+              margin: '0 8px'
+            }}
+            onClick={() => prev()}
+          >
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
   )
 }
 export default App
