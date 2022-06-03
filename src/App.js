2b74b29f5a53732fdc16f7e9cc3fdea64fe52342
dono118 <dono118@126.com>
2022-06-03 10:25:09 +0800

    简单的竖直方向的步骤条

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
-import { Button, message, Steps } from 'antd'
-import { useState } from 'react'
+import { Steps } from 'antd'
 import 'antd/dist/antd.min.css'
-
 const { Step } = Steps
-const steps = [
-  {
-    title: 'First',
-    content: 'First-content'
-  },
-  {
-    title: 'Second',
-    content: 'Second-content'
-  },
-  {
-    title: 'Last',
-    content: 'Last-content'
-  }
-]
 
-const App = () => {
-  const [current, setCurrent] = useState(0)
+const App = () => (
+  <Steps direction="vertical" current={1}>
+    <Step title="Finished" description="This is a description." />
+    <Step title="In Progress" description="This is a description." />
+    <Step title="Waiting" description="This is a description." />
+  </Steps>
+)
 
-  const next = () => {
-    setCurrent(current + 1)
-  }
-
-  const prev = () => {
-    setCurrent(current - 1)
-  }
-
-  return (
-    <>
-      <Steps current={current}>
-        {steps.map(item => (
-          <Step key={item.title} title={item.title} />
-        ))}
-      </Steps>
-      <div className="steps-content">{steps[current].content}</div>
-      <div className="steps-action">
-        {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()}>
-            Next
-          </Button>
-        )}
-        {current === steps.length - 1 && (
-          <Button
-            type="primary"
-            onClick={() => message.success('Processing complete!')}
-          >
-            Done
-          </Button>
-        )}
-        {current > 0 && (
-          <Button
-            style={{
-              margin: '0 8px'
-            }}
-            onClick={() => prev()}
-          >
-            Previous
-          </Button>
-        )}
-      </div>
-    </>
-  )
-}
 export default App
